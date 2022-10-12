84104fac9ed36e1f021473113cf2e0c6c77fe6ff
Jason Ling <Jasonling1996@gmail.com>
2022-10-12 04:36:15 +0000

    Updated work

--- vampire.js
+++ vampire.js
@@ -6,27 +6,42 @@ class Vampire {
     this.creator = null;
   }
 
+
   /** Simple tree methods **/
 
   // Adds the vampire as an offspring of this vampire
   addOffspring(vampire) {
-
+    this.offspring.push(vampire);
+    vampire.creator = this;
   }
 
   // Returns the total number of vampires created by that vampire
   get numberOfOffspring() {
-
+    return this.offspring.length;
   }
 
   // Returns the number of vampires away from the original vampire this vampire is
   get numberOfVampiresFromOriginal() {
+    let currentVampire = this;
+    let lineage = 0;
 
+    while (currentVampire.creator) {
+      currentVampire = currentVampire.creator;
+      lineage ++;
+    }
+    return lineage;
   }
 
   // Returns true if this vampire is more senior than the other vampire. (Who is closer to the original vampire)
   isMoreSeniorThan(vampire) {
+    let vampire1 = this;
+    let vampire2 = vampire;
 
-  }
+    if (vampire1.numberOfVampiresFromOriginal < vampire2.numberOfVampiresFromOriginal) {
+      return true;
+    }
+    return false;
+  };
 
   /** Stretch **/
 
@@ -35,10 +50,29 @@ class Vampire {
   // For example:
   // * when comparing Ansel and Sarah, Ansel is the closest common anscestor.
   // * when comparing Ansel and Andrew, Ansel is the closest common anscestor.
-  closestCommonAncestor(vampire) {
-
-  }
+  // closestCommonAncestor(vampire) {
+  // };
 }
+
+const original = new Vampire("Original", 1800);
+
+const ansel = new Vampire("Ansel",1850);
+const bart = new Vampire("Bart", 1855);
+
+const elgort = new Vampire("Elgort", 1900);
+const sarah = new Vampire("Sarah", 1915);
+
+const andrew = new Vampire("Andrew", 1921);
+
+original.addOffspring(ansel);
+original.addOffspring(bart);
+
+ansel.addOffspring(elgort);
+ansel.addOffspring(sarah);
+
+elgort.addOffspring(andrew);
+
+console.log(elgort.numberOfOffspring);
 
 module.exports = Vampire;
 
